14dcd8c5640fac5456f495b4e36e81e3d1cec9c2
Sergei Kurochkin <kurochkinsergei95@gmail.com>
2018-05-18 20:48:14 +0300

    pin login

--- static/js/pin.js
+++ static/js/pin.js
@@ -1,4 +1,47 @@
 $(document).ready(() => {
+    var resp_example = {
+        AccountAmount: 0,
+        AccountBalance: 4900,
+        AccountID: 1,
+        AccountParam1: null,
+        AccountParam2: null,
+        AccountParam3: null,
+        AccountParam4: null,
+        AccountPayment: 0,
+        AccountPrice: null,
+        AccountStatus: "0",
+        AccountType: "card",
+        AccountValidFrom: null,
+        AccountValidTo: null,
+        CardCVC: null,
+        CardCashLImit: null,
+        CardID: 1,
+        CardNumber: "2569128956431209",
+        CardParam1: "IL'YA MARSHAKOV",
+        CardParam2: null,
+        CardParam3: null,
+        CardParam4: null,
+        CardPin: 1000,
+        CardStatus: "manufactured",
+        CardType: null,
+        CardType2: null,
+        CardValidFrom: null,
+        CardValidTo: "2022-03-31 00:00:00.0000000",
+        IndivID: 16001,
+        LoyaltyScore: null,
+        ProdDetID: 8,
+        TMP_BLOCK_DTTM: 0,
+        TMP_BLOCK_FLG: 0,
+        TRUSTED_ATM_ENABLED: 0,
+        Trans_Limit: 0,
+        Trans_Limit_Flg: 0,
+        TrustedTransLimit: 0,
+        CardBlocked: 0
+    }
+    var $pinForm = $('.js-pin-form'),
+        $pinInput = $('.js-pin-input', $pinForm),
+        $buttons = $('.js-pin-button', $pinForm)
+
     readCookie = (name) => {
         var nameEQ = name + "=";
         var ca = document.cookie.split(';');
@@ -14,45 +57,66 @@ $(document).ready(() => {
 
     $("#pin").html((readCookie('Lang') == 'rus') ? 'Введите Ваш ПИН-код' : 'Enter your PIN');
 
-    $("button").click(
-        function() {
-            if ($(this).hasClass("delete")) {
-                $("#num").val($("#num").val().slice(0, -1))
-            } else if ($(this).hasClass("accept")) {
-                ident();
-            } else {
-                if ($("#num").val().length == 4) {
-                    return;
-                }
-                $("#num").val($("#num").val() + $(this).html());
-            }
+    $buttons.on('click',
+        (e) => {
+            var $button = $(e.target),
+                $input = $('.js-pin-input', $pinForm)
 
+            var buttonValue = $button.attr('data-button-value'),
+                inputValue = $input.val()
+
+            if (buttonValue == 'del') {
+                $input.val(inputValue.slice(0, -1))
+            } else if (buttonValue == 'submit') {
+                if (inputValue.length == 4)
+                    enterTheATM(inputValue);
+            } else if (inputValue.length == 4) return
+            else
+                $input.val(inputValue + buttonValue);
         }
     );
 
-    var options = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-        mode: 'cors',
-        cache: 'default',
-        body: JSON.stringify({ pin: 1001 })
-    };
 
-    fetch('http:/10.20.1.35:6001/get_card', options).then(function(response) {
-        var contentType = response.headers.get("content-type");
-        if (contentType && contentType.includes("application/json")) {
-            return response.json();
-        }
-        throw new TypeError("Oops, we haven't got JSON!");
-    }).then(function(json) {
-        console.log(json)
-    }).catch(function(e) {
-        console.log("Error getting card_data", e)
-    });
+    enterTheATM = (pin) => {
+        var options = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            mode: 'cors',
+            cache: 'default',
+            body: JSON.stringify({ pin: pin })
+        };
+
+        fetch('http:/10.20.1.35:6001/get_card', options).then(function(response) {
+            var contentType = response.headers.get("content-type");
+            if (contentType && contentType.includes("application/json")) {
+                return response.json();
+            }
+            throw new TypeError("джисон не пришел-(");
+        }).then(function(json) {
+            if (!json) {
+                inputColor = anime({
+                    targets: '.js-pin-input',
+                    backgroundColor: [
+                        { value: '#f2f2f2' },
+                        { value: 'rgb(238, 0, 105)' },
+                        { value: '#f2f2f2' },
+                    ],
+                    easing: 'linear',
+                    direction: 'alternate',
+                    duration: 1500
+                });
+            } else {
+                console.log('CardBlocked', json.CardBlocked)
+            }
+            console.log(json)
+        }).catch(function(e) {
+            console.log("Error getting card_data", e)
+        });
+    }
 
     //Установка таймера бездействия пользователя
     // $.idleTimer(120000);
